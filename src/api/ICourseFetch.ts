fbe7a89a1421bc20a805f2b338c279fb32d48695
serajsas <serajaldinabosabbah@gmail.com>
2022-08-27 02:15:59 -0700

    refactored parser logic

--- src/api/ICourseFetch.ts
+++ src/api/ICourseFetch.ts
@@ -1,5 +1,19 @@
-import {CourseModel} from "../models/CourseModel";
+import {CourseModel} from "../models/Course/CourseModel";
+import {PreRequisitesModel} from "../models/Course/PreRequisitesModel";
 
 export interface ICourseFetch {
-    getCoursePrereqs(deptName: string, courseNumber: number):Promise<CourseModel>;
+    /**
+     * Finds the pre-requisites for a given course
+     * @param deptName
+     * @param courseNumber
+     */
+    getCoursePrereqs(deptName: string, courseNumber: number): Promise<PreRequisitesModel>;
+
+    /**
+     * Returns true if the given courses have all the required pre-requisites
+     * otherwise return false
+     * @param courses
+     */
+    doesHavePrereqs(courses: Array<CourseModel>): Promise<boolean>;
+
 }
